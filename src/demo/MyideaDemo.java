e7cd5dd025f70ffc5b0a2a1f8aad75599d113454
liqi7026 <1099316772@qq.com>
2021-03-09 20:44:28 +0800

    git测试001

---
+++ src/demo/MyideaDemo.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class MyideaDemo {
+    public static void main(String[] args) {
+        for(int i=0;i<10000;i++){
+            System.out.println(i+"hi idea and git");
+        }
+    }
+}
